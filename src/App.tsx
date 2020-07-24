c1e79cb61dda8f4b53e48e93cba4d5c6478fa2e8
Roger Oliveira <rogerpolvr@gmail.com>
2020-07-24 20:40:45 -0300

    Criando as primeiras rotas com TSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
-  return <h1>Hello World</h1>;
-}
+import Routes from "./routes";
+
+// BrowserRouter permite que o acesso das rotas seja feito pelo navegador.
+// Exemplo localhost:300/repository
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Dashboard from "../pages/Dashboard/";
+import Repository from "../pages/Repository/";
+
+//Switch mostra apenas UMA rota por vez e não todas.
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" component={Repository} />
+  </Switch>
+);
+
+export default Routes;
