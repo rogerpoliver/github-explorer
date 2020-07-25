a5f34b8bcb59c8e949154313cea8bd6294be4e9c
Roger Oliveira <rogerpolvr@gmail.com>
2020-07-25 00:48:05 -0300

    Finalizando o estilo do background

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
+import GlobalStyle from "./styles/global";
 import Routes from "./routes";
 
 // BrowserRouter permite que o acesso das rotas seja feito pelo navegador.
 // Exemplo localhost:300/repository
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
