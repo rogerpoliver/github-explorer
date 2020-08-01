36ff59ab5940e6ef51465fee8c4b2ccc595915b1
Roger Oliveira <rogerpolvr@gmail.com>
2020-08-01 00:01:44 -0300

    Salvando os repositorios no localStoraged

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import api from '../../services/api';
 import logo from '../../assets/logo.svg';
 import { FiChevronRight } from 'react-icons/fi';
@@ -17,7 +17,24 @@ interface Repository {
 const Dashboard: React.FC = () => {
 	const [newRepo, setNewRepo] = useState('');
 	const [inputError, setInputError] = useState('');
-	const [repositories, setRepositories] = useState<Repository[]>([]);
+	const [repositories, setRepositories] = useState<Repository[]>(() => {
+		const storagedRepositories = localStorage.getItem(
+			'@GithubExplorer:repositories',
+		);
+
+		if (storagedRepositories) {
+			return JSON.parse(storagedRepositories);
+		}
+		return [];
+	});
+
+	// Dispara uma função sempre que o repositories tiver alteração
+	useEffect(() => {
+		localStorage.setItem(
+			'@GithubExplorer:repositories',
+			JSON.stringify(repositories),
+		);
+	}, [repositories]);
 
 	async function handleAddRepository(
 		event: FormEvent<HTMLFormElement>,
